26da4d3f656989a269dfdc3252e70f36cdec3326
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-01-05 14:29:20 -0300

    criação do projeto

---
+++ src/main/java/com/example/desafio_backend_anotaai/DesafioBackendAnotaaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_backend_anotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendAnotaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendAnotaaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_backend_anotaai/DesafioBackendAnotaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_backend_anotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendAnotaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
